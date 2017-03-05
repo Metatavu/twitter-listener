a24d30cb74442084b95bd0bb0a27e03a53821d73
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-03-06 00:10:41 +0200

    Added twitter implementation

---
+++ app.js
@@ -0,0 +1,42 @@
+(function () {
+  'use strict';
+
+  const request = require('request');
+  const Twitter = require('twitter');
+  const util = require('util');
+  const config = require(__dirname + '/config.json');
+  const commandLineArgs = require('command-line-args');
+
+  const optionDefinitions = [
+    { name: 'url', alias: 'u', type: String },
+    { name: 'tags', alias: 't', type: String, multiple: true }
+  ];
+
+  const options = commandLineArgs(optionDefinitions);
+
+  const client = new Twitter({
+    consumer_key: config.twitter.consumer_key,
+    consumer_secret: config.twitter.consumer_secret,
+    access_token_key: config.twitter.access_token_key,
+    access_token_secret: config.twitter.access_token_secret
+  });
+
+  client.stream('statuses/filter', { track: options.tags.join(',') }, stream => {
+    stream.on('data', tweet => {
+      console.log('Received tweet');
+
+      request(options.url, (error, response, body) => {
+        if(error) {
+          console.error(error);
+        } else {
+          console.log(util.format('Received [%s] %s from %s', response.statusCode, body, options.url));
+        }
+      });
+    });
+
+    stream.on('error', error => {
+      console.error(error);
+    });
+  });
+
+})();
